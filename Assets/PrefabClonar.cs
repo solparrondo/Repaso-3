80fe84b2d3de968f23e84a53108c5624ed5d9047
solparrondo <101563449+solparrondo@users.noreply.github.com>
2022-05-20 14:25:05 -0300

    Initial commit

---
+++ Assets/PrefabClonar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrefabClonar : MonoBehaviour
+{
+
+    public GameObject prefab;
+    public Text inputCloneAmount;
+    public int counter;
+    public Text displayCloneLeft;
+    public InputField iField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0;
+        displayCloneLeft.text = "?";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClonePrefab()
+    {
+        if(inputCloneAmount.text != "")
+        {
+            iField.readOnly = true;
+            int cloneAmount = int.Parse(inputCloneAmount.text);
+            if (counter < cloneAmount)
+            {
+                Instantiate(prefab);
+                counter++;
+                displayCloneLeft.text = (cloneAmount - counter).ToString();
+            }
+        }
+        else
+        {
+            displayCloneLeft.text = "ERROR. Ingrese un valor";
+        }
+    }
+
+       
+}
